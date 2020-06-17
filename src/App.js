b6c6cda3a22f8b8eecb74f022b30ff952203841b
Moroyzik <white_bear1902@mail.ru>
2020-06-17 17:43:14 +0300

    table and data for neoflex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleTable from "./table";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <SimpleTable/>
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
